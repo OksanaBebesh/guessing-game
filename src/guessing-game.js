1ae0050848498d3b540b42f1822dda289639ba70
OksanaBebesh <o.bebesh@itransition.com>
2023-10-10 16:32:21 +0200

    add: add one of solutions. not all tests work

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,34 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.arrayOfNumbers = [];
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.arrayOfNumbers = Array.from(
+      { length: max - min + 1 },
+      (_, i) => min + i
+    );
+    this.min = min;
+    this.max = max;
+    return this.arrayOfNumbers;
+  }
 
-    }
+  guess() {
+    this.resultGuess = Math.floor(
+      Math.random() * (this.max - this.min + 1) + this.min
+    );
+    return this.resultGuess;
+  }
 
-    guess() {
+  lower() {
+    let middle = Math.round((this.max - this.min) / 2) + this.min;
+    this.max = middle + 1;
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    let middle = Math.round((this.max - this.min) / 2) + this.min;
+    this.min = middle - 1;
+  }
 }
 
 module.exports = GuessingGame;
